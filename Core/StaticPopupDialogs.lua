a6a2a439958eaf94e81aeb6f187dbd81665d67d4
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-03-18 00:26:27 +0200

    Update popup editbox to be readonly

--- Core/StaticPopupDialogs.lua
+++ Core/StaticPopupDialogs.lua
@@ -1,3 +1,10 @@
+local function OnTextChanged(self)
+    self:SetText(self.originalText)
+    self:SetCursorPosition(0)
+    self:SetFocus()
+    self:HighlightText()
+end
+
 StaticPopupDialogs["TOOLTIPINFO_COPY_ID"] = StaticPopupDialogs["TOOLTIPINFO_COPY_ID"] or {
     text = "Copy %s.",
     button2 = CLOSE,
@@ -5,8 +12,13 @@ StaticPopupDialogs["TOOLTIPINFO_COPY_ID"] = StaticPopupDialogs["TOOLTIPINFO_COPY
     editBoxWidth = 320,
     closeButton = true,
     OnShow = function(self, data)
+        self.editBox.originalText = data
         self.editBox:SetText(data)
         self.editBox:HighlightText()
+        self.editBox:SetScript("OnTextChanged", OnTextChanged)
+    end,
+    OnHide = function(self)
+        self.editBox:SetScript("OnTextChanged", nil)
     end,
     EditBoxOnEnterPressed = HideParentPanel,
     EditBoxOnEscapePressed = HideParentPanel,
