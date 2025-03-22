8b8773dd2f95e9fbd3b1464fbdd1e948baf72614
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-03-22 17:57:44 +0200

    Exclude the guild line
    This is more reliable than checking for line numbers.

--- Features/Class.lua
+++ Features/Class.lua
@@ -1,23 +1,28 @@
 local UnitIsPlayer = UnitIsPlayer
 local UnitClass = UnitClass
+local GetGuildInfo = GetGuildInfo
 
 local RAID_CLASS_COLORS = RAID_CLASS_COLORS
-
-local lineNumber = 2
 
 TooltipDataProcessor.AddLinePreCall(Enum.TooltipDataLineType.None, function(tooltip, lineData)
     if tooltip:IsForbidden() then return end
     if tooltip ~= GameTooltip then return end
 
     local _, unit = tooltip:GetUnit()
+ 
+    if unit and UnitIsPlayer(unit) then
+        local guildName = GetGuildInfo(unit)
 
-    lineNumber = lineNumber > tooltip:NumLines() and 2 or lineNumber + 1
-    
-    if unit and UnitIsPlayer(unit) and lineNumber > 2 then 
+        if guildName and lineData.leftText:find(guildName) then
+            return
+        end
+
         local className, classFilename = UnitClass(unit)
+
         if not className or not classFilename then
             return
         end
+        
         if lineData.leftText:find(className) then
             local classColor = RAID_CLASS_COLORS[classFilename]
             if classColor then
