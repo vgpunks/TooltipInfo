dfd2d1327501530907e91c9b4418ba5c6d3afb3c
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-03-22 18:00:03 +0200

    Update PvP
    1. Exclude the guild line.
    2. Add the PvP status to the name.

--- Features/PvP.lua
+++ Features/PvP.lua
@@ -1,16 +1,46 @@
-local PVP = PVP
+local UnitIsPlayer = UnitIsPlayer
+local UnitIsPVP = UnitIsPVP
+local UnitIsEnemy = UnitIsEnemy
+local UnitCanAttack = UnitCanAttack
+local GetGuildInfo = GetGuildInfo
 
-local lineNumber = 2
+local PVP = PVP
+local FACTION_BAR_COLORS = FACTION_BAR_COLORS
+
+local PVP_LABEL = " (" .. PVP .. ")"
+
+TooltipDataProcessor.AddLinePreCall(Enum.TooltipDataLineType.UnitName, function(tooltip, lineData)
+    if tooltip:IsForbidden() then return end
+    if tooltip ~= GameTooltip then return end
+
+    local unit = lineData.unitToken
+
+    if unit and UnitIsPlayer(unit) then
+        if UnitIsPVP(unit) then
+            local hostileColor = FACTION_BAR_COLORS[5] -- Green (friendly)
+            if UnitIsEnemy("player", unit) then
+                hostileColor = FACTION_BAR_COLORS[1] -- Red (hostile)
+            elseif UnitCanAttack("player", unit) then
+                hostileColor = FACTION_BAR_COLORS[4] -- Yellow (neutral)
+            end
+            lineData.leftText = lineData.leftText .. hostileColor:WrapTextInColorCode(PVP_LABEL)
+        end
+    end
+end)
 
 TooltipDataProcessor.AddLinePreCall(Enum.TooltipDataLineType.None, function(tooltip, lineData)
     if tooltip:IsForbidden() then return end
     if tooltip ~= GameTooltip then return end
 
     local _, unit = tooltip:GetUnit()
-
-    lineNumber = lineNumber > tooltip:NumLines() and 2 or lineNumber + 1
     
-    if unit and UnitIsPlayer(unit) and lineNumber > 2 then 
+    if unit and UnitIsPlayer(unit) then
+        local guildName = GetGuildInfo(unit)
+
+        if guildName and lineData.leftText:find(guildName) then
+            return
+        end
+
         if lineData.leftText == PVP then
             lineData.leftText = ""
         end
