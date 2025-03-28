851c535aaa4aaeaabb89b637c541bee7b0e72ea1
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-03-29 02:46:17 +0300

    Refactor Filter.lua to filter the level and PvP lines for all units

--- Features/Filter.lua
+++ Features/Filter.lua
@@ -1,8 +1,7 @@
 local PVP = PVP
 local LEVEL = LEVEL
-local PLAYER = PLAYER
 
-local PLAYER_PATTERN = "%(" .. PLAYER .. "%)"
+local LEVEL_PATTERN = "%(.+%)"
 
 TooltipDataProcessor.AddLinePreCall(Enum.TooltipDataLineType.None, function(tooltip, lineData)
     if tooltip:IsForbidden() then return end
@@ -11,18 +10,17 @@ TooltipDataProcessor.AddLinePreCall(Enum.TooltipDataLineType.None, function(tool
     local _, unit = tooltip:GetUnit()
     
     if unit and not lineData.isGuildLine then
-        if UnitIsPlayer(unit) then
-            if lineData.leftText:find(LEVEL) then
-                lineData.leftText = lineData.leftText:gsub(PLAYER_PATTERN, "")
-            else
-                local _, localizedFaction = UnitFactionGroup(unit)
-                if lineData.leftText == localizedFaction then
-                    lineData.leftText = ""
-                end
-            end
+        if lineData.leftText:find(LEVEL) then
+            lineData.leftText = lineData.leftText:gsub(LEVEL_PATTERN, "")
         end
         if lineData.leftText == PVP then
             lineData.leftText = ""  
+        end
+        if UnitIsPlayer(unit) then
+            local _, localizedFaction = UnitFactionGroup(unit)
+            if lineData.leftText == localizedFaction then
+                lineData.leftText = ""
+            end
         end
     end
 end)
