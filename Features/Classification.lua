eca231ea02478b5991cbf214c140c2ee8fd6f8a0
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-03-29 04:26:59 +0300

    Add Unit Classification
    Introduce classification for non-player units with distinct colors:
    - **Elite:** Gold
    - **Rare Elite:** Light Blue
    - **Rare:** Blue
    - **Boss:** Orange
    - **Minus:** Gray

---
+++ Features/Classification.lua
@@ -0,0 +1,38 @@
+local _, addon = ...
+
+local UnitIsPlayer = UnitIsPlayer
+local UnitClassification = UnitClassification
+
+local LEVEL = LEVEL
+
+local CLASSIFICATIONS_FORMAT = " (%s)"
+local CLASSIFICATIONS = {
+    elite = ARTIFACT_GOLD_COLOR:WrapTextInColorCode(CLASSIFICATIONS_FORMAT:format(ELITE)),
+    rareelite = HEIRLOOM_BLUE_COLOR:WrapTextInColorCode(CLASSIFICATIONS_FORMAT:format(MAP_LEGEND_RAREELITE)),
+    rare = RARE_BLUE_COLOR:WrapTextInColorCode(CLASSIFICATIONS_FORMAT:format(MAP_LEGEND_RARE)),
+    worldboss = LEGENDARY_ORANGE_COLOR:WrapTextInColorCode(CLASSIFICATIONS_FORMAT:format(BOSS)),
+    minus = COMMON_GRAY_COLOR:WrapTextInColorCode(CLASSIFICATIONS_FORMAT:format(UNIT_NAMEPLATES_SHOW_ENEMY_MINUS)),
+}
+
+TooltipDataProcessor.AddLinePreCall(Enum.TooltipDataLineType.None, function(tooltip, lineData)
+    if tooltip:IsForbidden() then return end
+    if tooltip ~= GameTooltip then return end
+    
+    local _, unit = tooltip:GetUnit()
+
+    if unit and not UnitIsPlayer(unit) then
+        local classification = UnitClassification(unit)
+		
+        if not classification then
+            return 
+        end
+        
+        if lineData.leftText:find(LEVEL) then
+            local classText = CLASSIFICATIONS[classification]
+
+            if classText then
+                lineData.leftText = lineData.leftText .. classText
+            end
+        end
+    end
+end)
